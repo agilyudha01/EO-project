80c697b346f1d06ce6d1995e5a829b5d0bd0787f
agilyudha01 <agilyudha01@gmail.com>
2024-10-02 00:33:01 +0700

    chenge user title

--- resources/views/user/layout/main.blade.php
+++ resources/views/user/layout/main.blade.php
@@ -9,7 +9,7 @@
     <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!-- Title  -->
-    <title>Essence - Fashion Ecommerce Template</title>
+    <title>Event Organizer</title>
 
     <!-- Favicon  -->
     <link rel="icon" href="{{ asset('assets/img/core-img/favicon.ico') }}">
