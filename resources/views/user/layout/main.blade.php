aabc7a3de7eae0b0324401efa10401aa89af49d5
agilyudha01 <agilyudha01@gmail.com>
2024-10-02 00:34:23 +0700

    removes junk code

--- resources/views/user/layout/main.blade.php
+++ resources/views/user/layout/main.blade.php
@@ -19,7 +19,6 @@
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bottomNavbar.css') }}">
 
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
     @yield('head')
